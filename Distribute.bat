f536571f3447dc62439b72926de29d396528af91
Colby Dillion <colby.dillion@gmail.com>
2010-11-02 16:26:36 -0500

    add Dicom XML documentation to distribution

--- Distribute.bat
+++ Distribute.bat
@@ -7,6 +7,7 @@ copy Dicom.Dump\bin\Release\*.dic Distribute\ /Y
 copy Dicom.Dump\bin\Release\*.txt Distribute\ /Y
 copy Dicom.Linq\bin\Release\*.dll Distribute\ /Y
 copy Dicom.Scu\bin\Release\*.exe Distribute\ /Y
+copy Dicom\bin\Release\Dicom.XML Distribute\ /Y
 copy "3rd Party\OpenSSL\*.dll" Distribute\ /Y
 del /F /Q Distribute\ssleay32.dll
 del /F /Q Distrubite\libeay32.dll
