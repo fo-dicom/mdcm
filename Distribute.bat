a00978fc3083743bb5e82c7652709f3ba949bbb3
Colby Dillion <colby.dillion@gmail.com>
2011-03-02 11:56:40 -0600

    add codecs and README to distribution

--- Distribute.bat
+++ Distribute.bat
@@ -7,7 +7,9 @@ copy Dicom.Dump\bin\Release\*.dic Distribute\ /Y
 copy Dicom.Dump\bin\Release\*.txt Distribute\ /Y
 copy Dicom.Linq\bin\Release\*.dll Distribute\ /Y
 copy Dicom.Scu\bin\Release\*.exe Distribute\ /Y
+copy Release\*.dll Distribute\ /Y
 copy Dicom\bin\Release\Dicom.XML Distribute\ /Y
+copy README Distribute\ /Y
 copy "3rd Party\OpenSSL\*.dll" Distribute\ /Y
 del /F /Q Distribute\ssleay32.dll
 del /F /Q Distrubite\libeay32.dll
