aced0ddc308902651a8a295a12b258d18aaddb84
cureos <anders@cureos.com>
2011-10-26 11:46:02 +0200

    Generalized DcmPixelData.GetFrameDataUx methods, added GetFrameDataU32 method and added unit tests to confirm maintained functionality of already existing methods

--- Wpf.Dicom.Tests/Properties/AssemblyInfo.cs
+++ Wpf.Dicom.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Wpf.Dicom")]
+[assembly: AssemblyTitle("Wpf.Dicom.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Cureos AB")]
-[assembly: AssemblyProduct("Wpf.Dicom")]
+[assembly: AssemblyProduct("Wpf.Dicom.Tests")]
 [assembly: AssemblyCopyright("Copyright © Cureos AB 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("82f4dae8-a1d7-4420-a497-5472c10564cc")]
+[assembly: Guid("b685d430-b77c-4dd3-a0b2-066a01d6f483")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,10 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("Wpf.Dicom.Tests, PublicKey=" +
-                              "0024000004800000940000000602000000240000525341310004000001000100e5fbac4e1806c8" +
-                              "a8a25403b276b1402a65d95cba1f741b6d7a7e144599887c90901da960daefa91ec0e43507859e" +
-                              "c63862b7937b3599ffc0fcfc53a0e62281e5201e097964bc8c8b0915c12e3876dbed15195fca52" +
-                              "f15d47689746f300413ecb88cee373e811dc264e05b71118cf0e6dc8dd7b82219fa726fa075fc6" +
-                              "c2fed7ac")]
