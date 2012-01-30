30fca3ea3366a97cef45d82b427c300ef3aeb1a0
cureos <anders@cureos.com>
2012-01-30 14:18:21 +0100

    Corrected GetList methods in DcmDecimalString and DcmIntegerString, and implemented more efficient Get array methods. Fixes issue #1

---
+++ Wpf.Dicom.Tests/Data/DcmDecimalStringTests.cs
@@ -0,0 +1,200 @@
+﻿/*
+ * Copyright (c) 2012  Anders Gustafsson, Cureos AB.
+ * This file is part of mdcm.
+ *
+ * mdcm is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * mdcm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with mdcm.  
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Dicom.Data
+{
+    [TestFixture]
+    public class DcmDecimalStringTests
+    {
+        #region Fields
+
+        private DcmDecimalString[] _instances;
+
+        #endregion
+
+        #region SetUp and TearDown
+
+        [SetUp]
+        public void Setup()
+        {
+            var ff = new DicomFileFormat();
+            ff.Load("Testdata/rtss.dcm", DicomReadOptions.DefaultWithoutDeferredLoading);
+            _instances = ff.Dataset.GetSQ(DicomTags.ROIContourSequence).SequenceItems.SelectMany(
+                item =>
+                (item.Dataset.GetSQ(DicomTags.ContourSequence) ?? new DcmItemSequence(DicomTags.ContourSequence)).
+                    SequenceItems).Select(item => item.Dataset.GetDS(DicomTags.ContourData)).ToArray();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _instances = null;
+        }
+
+        #endregion
+
+        #region Unit tests
+
+        [Test]
+        public void GetFloats_ContourData_IndividualElementCorrectlyRead()
+        {
+            const float expected = -336.55f;
+            var actual = _instances[0].GetFloats()[13];
+            Assert.AreEqual(expected, actual, 1.0e-3);
+        }
+
+        [Test]
+        public void GetFloats_ReadAllContourData_Timing()
+        {
+            var timer = new Stopwatch();
+            timer.Restart();
+            foreach (var ds in _instances)
+            {
+                var vertices = ds.GetFloats();
+            }
+            timer.Stop();
+
+            Assert.Pass("Elapsed time: {0} ms", timer.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public void GetFloatList_ContourData_IndividualElementCorrectlyRead()
+        {
+            const float expected = -336.55f;
+            var actual = _instances[0].GetFloatList()[13];
+            Assert.AreEqual(expected, actual, 1.0e-3);
+        }
+
+        [Test]
+        public void GetFloatList_ReadAllContourData_Timing()
+        {
+            var timer = new Stopwatch();
+            timer.Restart();
+            foreach (var ds in _instances)
+            {
+                var vertices = ds.GetFloatList();
+            }
+            timer.Stop();
+
+            Assert.Pass("Elapsed time: {0} ms", timer.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public void GetDoubles_ContourData_IndividualElementCorrectlyRead()
+        {
+            const double expected = -336.55;
+            var actual = _instances[0].GetDoubles()[13];
+            Assert.AreEqual(expected, actual, 1.0e-7);
+        }
+
+        [Test]
+        public void GetDoubles_ReadAllContourData_Timing()
+        {
+            var timer = new Stopwatch();
+            timer.Restart();
+            foreach (var ds in _instances)
+            {
+                var vertices = ds.GetDoubles();
+            }
+            timer.Stop();
+
+            Assert.Pass("Elapsed time: {0} ms", timer.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public void GetDoubleList_ContourData_IndividualElementCorrectlyRead()
+        {
+            const double expected = -336.55;
+            var actual = _instances[0].GetDoubleList()[13];
+            Assert.AreEqual(expected, actual, 1.0e-7);
+        }
+
+        [Test]
+        public void GetDoubleList_ReadAllContourData_Timing()
+        {
+            var timer = new Stopwatch();
+            timer.Restart();
+            foreach (var ds in _instances)
+            {
+                var vertices = ds.GetDoubleList();
+            }
+            timer.Stop();
+
+            Assert.Pass("Elapsed time: {0} ms", timer.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public void GetDecimals_ContourData_IndividualElementCorrectlyRead()
+        {
+            const decimal expected = -336.55m;
+            var actual = _instances[0].GetDecimals()[13];
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetDecimals_ReadAllContourData_Timing()
+        {
+            var timer = new Stopwatch();
+            timer.Restart();
+            foreach (var ds in _instances)
+            {
+                var vertices = ds.GetDecimals();
+            }
+            timer.Stop();
+
+            Assert.Pass("Elapsed time: {0} ms", timer.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public void GetDecimalList_ContourData_IndividualElementCorrectlyRead()
+        {
+            const decimal expected = -336.55m;
+            var actual = _instances[0].GetDecimalList()[13];
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetDecimalList_ReadAllContourData_Timing()
+        {
+            var timer = new Stopwatch();
+            timer.Restart();
+            foreach (var ds in _instances)
+            {
+                var vertices = ds.GetDecimalList();
+            }
+            timer.Stop();
+
+            Assert.Pass("Elapsed time: {0} ms", timer.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public void GetDecimal_ContourData_IndividualElementCorrectlyRead()
+        {
+            const decimal expected = 0.54m;
+            var actual = _instances[0].GetDecimal(612);
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+    }
+}
---
+++ Wpf.Dicom.Tests/Data/DcmIntegerStringTests.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Copyright (c) 2012  Anders Gustafsson, Cureos AB.
+ * This file is part of mdcm.
+ *
+ * mdcm is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * mdcm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with mdcm.  
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Globalization;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Dicom.Data
+{
+    [TestFixture]
+    public class DcmIntegerStringTests
+    {
+        #region Fields
+
+        private DcmIntegerString _instance;
+
+        #endregion
+
+        #region SetUp and TearDown
+
+        [SetUp]
+        public void Setup()
+        {
+            var rnd = new Randomizer();
+            _instance = new DcmIntegerString(DicomTags.ReferencedFrameNumber);
+            _instance.SetValues(
+                rnd.GetInts(0, 65535, 5000).Select(i => i.ToString(CultureInfo.InvariantCulture)).
+                Concat(new[] { "32767" }).ToArray());
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _instance = null;
+        }
+
+        #endregion
+
+        #region Unit tests
+
+        [Test]
+        public void GetInt32s_LastElement_ReturnsCorrectValue()
+        {
+            const int expected = 32767;
+            var actual = _instance.GetInt32s().Last();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetInt32List_LastElement_ReturnsCorrectValue()
+        {
+            const int expected = 32767;
+            var actual = _instance.GetInt32List().Last();
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+    }
+}
